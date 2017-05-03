f70ab40315b977edb065382dab296c8410914ed6
Agnaldo Marinho <agnaldomarinho7@gmail.com>
2017-05-03 16:45:21 -0300

    adcionando a quarta questao

---
+++ exercio/quest04.go
@@ -0,0 +1,16 @@
+/*
+*Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+*/
+package main
+
+import "fmt"
+
+func main(){
+	var a, b, c, d int
+	fmt.Println("Entre com 4 notas bimestrais: ")
+	fmt.Scan(&a, &b, &c, &d)
+	media := (a+b+c+d)/4
+	fmt.Println("A media é: ", media)
+}
+
+
