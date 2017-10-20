e275c2c402079567144a1cd8a7a1ad19a14e7ce5
Agnaldo Marinho <agnaldoneto@ufpa.br>
2017-10-20 02:09:34 -0300

    Adcionando template/index.html e executando o template

--- web/main.go
+++ web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -14,7 +15,10 @@ type Post struct {
 func main() {
 	// Difinir rota
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Curso de Golang para Web")
+		// Iniciando template
+		t := template.Must(template.ParseFiles("templates/index.html"))
+		// Executar o template
+		t.ExecuteTemplate(w, "index.html", nill)
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
