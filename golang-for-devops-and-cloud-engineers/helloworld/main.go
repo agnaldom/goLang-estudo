73a36f7037aed2752f60805dbc331647e7340a3f
agnaldonm <agnaldo.marinho@galp.com>
2023-09-27 21:54:47 -0300

    Using Command arguments With conditionais

--- golang-for-devops-and-cloud-engineers/helloworld/main.go
+++ golang-for-devops-and-cloud-engineers/helloworld/main.go
@@ -7,5 +7,11 @@ import (
 
 func main() {
 	args := os.Args
-	fmt.Printf("Hello World\nos.Args: %v\nArguments: %v\n", args, args[1:len(args)])
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./hello-world <argument>\n")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Hello World\nos.Args: %v\nArguments: %v\n", args, args[1:])
 }
