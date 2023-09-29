7b5b2a35706d0273226ef7a16b706259ea5d5dff
agnaldonm <agnaldo.marinho@galp.com>
2023-09-28 21:45:12 -0300

    HTTP Get from API

--- golang-for-devops-and-cloud-engineers/helloworld/main.go
+++ golang-for-devops-and-cloud-engineers/helloworld/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -9,9 +13,29 @@ func main() {
 	args := os.Args
 
 	if len(args) < 2 {
-		fmt.Printf("Usage: ./hello-world <argument>\n")
+		fmt.Printf("Usage: ./http-get <argument>\n")
 		os.Exit(1)
 	}
 
-	fmt.Printf("Hello World\nos.Args: %v\nArguments: %v\n", args, args[1:])
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
+
 }
