e90df5ab75220a88b4593e57283842b9829a354c
Agnaldo Marinho <agnaldo.marinho.cev@elo.com.br>
2023-10-03 10:22:41 -0300

    The simples refactor for this would be to extract out some functionality into another function

--- learn-go-with-tests/hello-world/hello.go
+++ learn-go-with-tests/hello-world/hello.go
@@ -2,34 +2,34 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	/*if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name*/
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
 
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
-	case "French":
+	case french:
 		prefix = frenchHelloPrefix
-	case "Spanish":
+	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
